1845d67cd3ad112365655b4fddf2c2eb30baa601
edwinc456 <skydude51@yahoo.com>
2014-05-18 23:21:11 -0700

    Update maindice.java

--- maindice.java
+++ maindice.java
@@ -15,6 +15,6 @@ public static void main(String[]args)
 	
 	number.trippledicewithhistory();
 	copy2.trippledicewithhistory();
-	System.out.println(number.equals(copy));
+	System.out.println(number.equals(copy2));
 }
 }
